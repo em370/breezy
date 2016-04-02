64c542c1d697e76b8f37eaf91717d59a77e492b5
em370 <ermi1253@colorado.edu>
2016-04-01 23:14:09 -0600

    chat page format

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = require('express')();
+var uuid= require('tower-uuid');
 var server = require('http').createServer(app);
 var port = process.env.PORT || 3000;
 app.use(express.static('public'));
 
-
+console.log(uuid());
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/public/html/home.html');
 });
