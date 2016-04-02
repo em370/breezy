9e8c0e5817b80536fc18f7d485f7c80828b4dca4
Jared Jolton <jared.jolton@gmail.com>
2016-04-02 00:18:10 -0600

    pair programming with raylyn: added event listener for user signup

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,9 +1,22 @@
 var Firebase = require('Firebase');
 var ref = new Firebase("https://breezytalk.firebaseio.com");
 
-ref.createUser({
-  email    : "bobtony@firebase.com",
-  password : "correcthorsebatterystaple"
-});
+console.log('the file ran!');
 
+var submit = document.getElementById("submit-button");
+console.log(submit);
+
+submit.addEventListener('click', function() {
+    alert('Hello world');
+    ref.createUser({
+      email    : "bobtony@firebase.com",
+      password : "correcthorsebatterystaple"
+    }, function(error, userData) {
+      if (error) {
+        console.log("Error creating user:", error);
+      } else {
+        console.log("Successfully created user account with uid:", userData.uid);
+      }
+    });
+}, false);
 
