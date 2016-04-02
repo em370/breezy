292715175a19f60b8da139f53ae8f7689bfce8ac
Jared Jolton <jared.jolton@gmail.com>
2016-04-02 15:33:30 -0600

    added about us button, formatting changes on lots of stuff

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ var translate = require('yandex-translate')(key);
 app.use(express.static('public'));
 var defaultNsps = '/';
 console.log(uuid());
+
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/public/html/home.html');
 });
-
 
 app.get('/lang', function(req, res){
 	res.sendFile(__dirname + '/public/html/lang.html');
@@ -45,13 +45,6 @@ app.get('/wait', function(req, res){
 app.get('/forgotten', function(req, res){
 	res.sendFile(__dirname + '/public/html/forgotten.html');
 });
-
-app.get('/fix', function(req, res){
-	res.sendFile(__dirname + '/public/html/fix.html');
-});
-
-
-
 
 io.sockets.on('connection', function(socket){
 	console.log('a user connected');
@@ -94,11 +87,8 @@ io.sockets.on('connection', function(socket){
 			console.log(res.text);
 			socket.emit('translated', {name: data.name, message: res.text,room: data.room});
 		});
-	});
-	
-	
+	});	
 });
-
 
 server.listen(port, function(){
   console.log('listening on port: '+ port);
